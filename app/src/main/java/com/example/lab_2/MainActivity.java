99607745b67ad01daccc7f9fb455730dc9386226
YoKawaiiK <danil.legioner@gmail.com>
2020-12-19 19:50:36 +0700

    4 Пункт

--- app/src/main/java/com/example/lab_2/MainActivity.java
+++ app/src/main/java/com/example/lab_2/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.lab_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView id_phrase; // Поле для значения процента
 
     // Создание объекта класса Phraser
     private Phraser phraser = new Phraser();
@@ -18,10 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        id_phrase = findViewById(R.id.id_phrase);
+        // Получаем элементы
+        TextView id_phrase = (TextView) findViewById(R.id.id_phrase); // Поле
+        Button id_genButton = (Button) findViewById(R.id.id_genButton);
 
-        String text_phrase = phraser.PhraserGen();
 
-        id_phrase.setText(text_phrase);
+        // Установить начальную фразу
+        id_phrase.setText(phraser.PhraserGen());
+
+        id_genButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                String text_phrase = phraser.PhraserGen();
+
+                id_phrase.setText(text_phrase);
+            }
+        });
     }
+
+
 }
